c04bd21f3326494b8aed34ef69c311cac4ab361e
beccauwu <rebecca@perttula.co>
2022-08-31 19:21:51 +0200

    fix issues

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/ash
 
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $DB_HOST $DB_PORT; do
+    while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
 
