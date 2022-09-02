78c86d391791565e72a349b071f37a65bb6e22d9
beccauwu <rebecca@perttula.co>
2022-09-02 16:45:56 +0200

    rm app directory

--- app/payments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cancel/', views.CancelView.as_view(), name='cancel'),
-    path('success/', views.SuccessView.as_view(), name='success'),
-    path('create-checkout-session/', views.CreateCheckoutSessionView.as_view(), name='create-checkout-session')
-]
